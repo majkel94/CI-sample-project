6d9b7271e62ce139bf6209aaed3529fb2cc2fa5d
Kuba <niezasniesz@gmail.com>
2018-07-12 20:10:12 +0200

    change project structure

--- src/main.py
+++ src/main.py
@@ -1,33 +1,10 @@
-"""
-Module to train autogenerating docs
-"""
-def inc(arg):
-    """
-    Function increment given argument
+from flask import Flask
 
-    >>> inc(1)
-    2
+import json
 
-    :param arg: Argument to increment
-    :return: arg + 1
-    :rtype: integer
-    """
-    return arg+1
+app = Flask(__name__)
 
 
-def iDontCare(nick, *, x=1):
-    """
-    Function greet given person
-    
-    >>> iDontCare('Cukierku')
-    'Hej Cukierku:*'
-    
-    :param nick: Nickname to greet
-    :param x: Number of greetings (default=1)
-    :return: Greeting text
-    :rtype: string
-    """
-    return f"Hej {nick}:* "*x
-
-def new_cool_func():
-    return 'Stickler'
+@app.route('/', methods=['GET'])
+def main():
+    return json.dumps("{'foo': 'bar'}")
--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,13 @@
 from src import main
 
+
 def test_sample():
     assert main.inc(3) == 4
 
+
 def test_fail():
     assert main.inc(2) != 6
+
 
 def test_new_cool_func_returns_stickler():
     assert main.new_cool_func() == 'Stickler'
